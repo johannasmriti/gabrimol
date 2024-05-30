ea242f63bcd32b14ca413cfcac296d30a017b0be
johannasmriti <19z218@psgtech.ac.in>
2024-05-30 09:04:48 +0530

    build

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{b as a}from"./chunk-FOKJXRJR.mjs";import"./chunk-EPE66GTQ.mjs";import"./chunk-C4NOQX2E.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{b as a}from"./chunk-TV6IXH4N.mjs";import"./chunk-EPE66GTQ.mjs";import"./chunk-C4NOQX2E.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
