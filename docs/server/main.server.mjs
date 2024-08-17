2d2660497ba1855789e0b57b6698dcb5b05b58b3
Johanna Smriti <64794456+johannasmriti@users.noreply.github.com>
2024-08-17 22:08:35 +0530

    adding dist

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{b as a}from"./chunk-BNJLNR3B.mjs";import"./chunk-EPE66GTQ.mjs";import"./chunk-C4NOQX2E.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{b as a}from"./chunk-VA5OWQMA.mjs";import"./chunk-EPE66GTQ.mjs";import"./chunk-C4NOQX2E.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
